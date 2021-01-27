152c85cec98accce9953f8f93d6591feb5a78c23
Elysiayn <wendy.lemus.wl@gmail.com>
2021-01-26 20:43:55 -0500

    modularized code

--- server.js
+++ server.js
@@ -1,79 +1,18 @@
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
 const db = require('./db/database');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const apiRoutes = require('./routes/apiRoutes');
-app.use('/api', apiRoutes);
-
 // Express middleware
 app.use(express.urlencoded({
     extended: false
 }));
 app.use(express.json());
 
-
-//*************************************
-// *********** Party routes ***********
-//*************************************
-// Get all parties
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            res.status(500).json({
-                error: err.message
-            });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-// Get single party
-app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, rows) => {
-        if (err) {
-            res.status(400).json({
-                error: err.message
-            });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-// Delete a party
-app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id]
-    db.run(sql, params, function (err, result) {
-        if (err) {
-            res.status(400).json({
-                error: res.message
-            });
-            return;
-        }
-
-        res.json({
-            message: 'successfully deleted',
-            changes: this.changes
-        });
-    });
-});
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
 // Default response for any other request(Not Found) Catch all other
 app.use((req, res) => {
