7c0b229e8c6a53b591fe5809113dd3393f70e448
Elysiayn <wendy.lemus.wl@gmail.com>
2021-01-26 22:04:15 -0500

    updated code w/ codesnap

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,31 @@
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
-<<<<<<< HEAD
-DROP TABLE IF EXISTS votes;
-=======
 DROP TABLE IF EXISTS voters;
->>>>>>> 7b43f03c1041fe5b54fb652647f77656e8661e65
+DROP TABLE IF EXISTS votes;
 
--- table for parties
 CREATE TABLE parties (
   id INTEGER PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   description TEXT
 );
 
--- table for candidates
 CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL,
   party_id INTEGER UNSIGNED,
+  industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
 
-<<<<<<< HEAD
+CREATE TABLE voters (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE votes (
   id INTEGER PRIMARY KEY,
   voter_id INTEGER UNSIGNED NOT NULL,
@@ -32,13 +34,4 @@ CREATE TABLE votes (
   CONSTRAINT uc_voter UNIQUE (voter_id),
   CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
   CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
-=======
--- table for voters
-CREATE TABLE voters (
-  id INTEGER PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
->>>>>>> 7b43f03c1041fe5b54fb652647f77656e8661e65
 );
